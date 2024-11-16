2d88aac39ed846227ed5dd74392d1f477e6fe372
Ivan Amoshyi <amoshyiivan@gmail.com>
2024-11-16 23:04:27 +0200

    implemented scrollToTop button

--- src/main.js
+++ src/main.js
@@ -2,15 +2,19 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchImages from './js/pixabay-api';
 import createMarkUp from './js/render-functions';
-
 import * as notification from './utils/notifications';
+import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.js-form');
 const gallery = document.querySelector('.js-gallery');
 const loadButton = document.querySelector('.js-load-button');
+const scrollButton = document.querySelector('.scroll-up-button');
 
 let page;
 let searchQuery;
+
+console.log(throttle);
+
 
 const lightbox = new SimpleLightbox('.item-link', {
   captions: true,
@@ -20,6 +24,26 @@ const lightbox = new SimpleLightbox('.item-link', {
 
 form.addEventListener('submit', handleSearch);
 loadButton.addEventListener('click', handleLoadMore);
+
+scrollButton.addEventListener('click', () => {
+  window.scroll({
+    top: 0,
+    behavior: 'smooth'
+  })
+})
+
+window.addEventListener(
+  'scroll',
+  throttle(() => {
+    console.log('scrolled');
+    if (window.scrollY > 2400) {
+      scrollButton.classList.remove('scroll-hidden');
+    } else {
+      scrollButton.classList.add('scroll-hidden');
+    }
+  }, 300)
+);
+
 
 function handleSearch(e) {
   e.preventDefault();
